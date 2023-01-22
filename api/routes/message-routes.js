5cb704787167d7cc1c4c2d68f249e2cee623225f
unknown <khadkasuman324@gmail.com>
2023-01-22 12:56:23 +0545

    get messages and add message functionality for API added

--- api/routes/message-routes.js
+++ api/routes/message-routes.js
@@ -5,8 +5,27 @@ const Message = require("../model/Message");
 
 
 //new message
+router.post("/", async (req, res)=> {
+    const newMessage = new Message(req.body);
+
+    try {
+        savedMessage = await newMessage.save();
+        res.status(200).json(savedMessage);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 //get messages
+router.get("/:conversationId", async(req, res)=> {
+
+    try {
+        const messages =await Message.find({conversationId: req.params.conversationId});
+        res.status(200).json(messages);
+    } catch (error) {
+        res.status(500).json(error);
+    }  
+});
 
 
 module.exports = router;
